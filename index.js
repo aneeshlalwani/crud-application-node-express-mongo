f18d8c8a7cc8d474479bd86e1dcdbac30d5ca483
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-18 15:07:18 +0500

    connected database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* This file is the BRAIN of Server */
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // get
 app.get('/', (req, res) => {
@@ -9,6 +11,14 @@ app.get('/', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
+// connecting database
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log('DB Connected...');
+    app.listen(3000, () => {
     console.log('server listening on port 3000......');
+})}).catch(()=>{
+    console.log('Connection Failed!');
 })
+
+
