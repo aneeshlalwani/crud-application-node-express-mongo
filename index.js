962b04d8160019dd4d835bbddf0fce4e7a17fe20
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-18 16:24:57 +0500

    created post api

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// get
+const Product = require('./models/product.model.js');
+
+app.use(express.json());
+
+// GET
 app.get('/', (req, res) => {
     res.json({
         "name": "aneesh",
     });
 })
 
+// POST API
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({messgae: error.message});
+    }
+})
 // connecting database
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
