c0534976cf0ff57f95c36001fd2eb9b55c7e003e
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-02-19 22:34:21 +0500

    delete api to delete a particular product

--- index.js
+++ index.js
@@ -52,6 +52,22 @@ app.put('/api/product/:id', async (req, res) =>{
         res.status(500).json({message: error});
     }
 })
+
+//  DELETE API TO DELETE A PARTICULAR PRODUCT
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json({message: 'Product deleted successfully'});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
 // POST API
 app.post('/api/products', async (req, res) => {
     try {
